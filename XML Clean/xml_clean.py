3ebd6d6796898d07c02462c6db3cb5fba4b6a266
beirtipol <beirtipol@gmail.com>
2019-04-26 15:30:34 +0100

    Initial Import

---
+++ XML Clean/xml_clean.py
@@ -0,0 +1,9 @@
+import sublime
+import sublime_plugin
+
+class XmlClean(sublime_plugin.TextCommand):
+	def run(self, edit):
+		regions = self.view.find_all( '><' )
+		for region in reversed(regions):
+			self.view.replace(edit, region, '>\n<' )
+		self.view.run_command("reindent", {"single_line": False})
