b3f3338b5ac3a7f123b2cb997e45f5c26915b695
Maciej Czubak <Maciej.Czubak@bluecrestcapital.com>
2019-09-06 14:27:32 +0100

    Changing xml to upper case to avoid imaginary compatibility issues

--- XML Clean/xml_clean.py
+++ XML Clean/xml_clean.py
@@ -3,7 +3,7 @@ import sublime_plugin
 
 class XmlClean(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.assign_syntax('xml.sublime-syntax')
+		self.view.assign_syntax('XML.sublime-syntax')
 		regions = self.view.find_all( '><' )
 		for region in reversed(regions):
 			self.view.replace(edit, region, '>\n<' )
