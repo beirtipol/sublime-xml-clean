751bb27f0bd08da00149163db50c811f2f4392d8
Maciej Czubak <Maciej.Czubak@bluecrestcapital.com>
2019-09-06 14:18:26 +0100

    Set syntax as xml before reindent. Removing pyc file

--- XML Clean/xml_clean.py
+++ XML Clean/xml_clean.py
@@ -3,6 +3,7 @@ import sublime_plugin
 
 class XmlClean(sublime_plugin.TextCommand):
 	def run(self, edit):
+		self.view.assign_syntax('xml.sublime-syntax')
 		regions = self.view.find_all( '><' )
 		for region in reversed(regions):
 			self.view.replace(edit, region, '>\n<' )
